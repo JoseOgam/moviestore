4ca66c7e265a1f170acc602a80acf94455c8627b
Jose <jotienoochieng@gmail.com>
2019-04-09 19:49:07 +0300

    second commit

--- contacts/settings.py
+++ contacts/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+INSTALLED_APPS = (
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.core',
-]
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- core/models.py
+++ core/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Movie(models.models):
+    NOT_RATED = 0
+    RATED_G = 1
+    RATED_PG = 2
+    RATED_R = 3
+    RATINGS = (
+        (NOT_RATED, 'NR - Not Rated'),
+        (RATED_G, 'G - General Audiences'),
+        (RATED_PG, 'PG - Parental Guidance' 'Suggested'),
+        (RATED_R, 'R - Restricted'),
+
+    )
+
+    title = models.CharField(
+        max_length=140)
+    plot = models.TextField()
+    year = models.PositiveIntegerField()
+    rating = models.IntegerField(
+        choices=RATINGS,
+        default=NOT_RATED)
+
+    models.PositiveIntegerField()
+    website = models.URLField(
+
+         blank=True )
+
+    def __str__(self):
+
+        return '{} ({})' .format(self.title, self.year)
